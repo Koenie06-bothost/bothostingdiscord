90ad89246ec9a59264ab6f7b35d38dfc8397861b
Koenie06 <koentermaat1@gmail.com>
2020-09-27 18:28:38 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -4,9 +4,10 @@ const fs = require('fs');
 const ms = require('ms');
 
 module.exports.run = async (client, message, args) => {
-    
+
             if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You can\'t use this command!')
             if(!args[1]) return message.reply('Say how many messages you want to delete!').then(msg => msg.delete({ timeout: 5000}));
+            message.delete()
             message.channel.bulkDelete(args[1]);
 
 }
