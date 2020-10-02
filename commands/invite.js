9724368c39fe1d6e85e72529d3a8289b61050b7f
Koenie06 <koentermaat1@gmail.com>
2020-10-02 15:32:15 +0200

    Create invite.js

---
+++ commands/invite.js
@@ -0,0 +1,21 @@
+const discord = require('discord.js');
+const bot = new discord.Client();
+const fs = require('fs');
+const ms = require('ms');
+
+module.exports.run = async (client, message, args) => {
+
+    var messageEmbed = new discord.MessageEmbed()
+     .setColor("RANDOM")
+     .setTitle(`Invite`)
+     .setDescription(`You want to add me to your server? Sure!
+     
+     Click [here](https://discord.com/api/oauth2/authorize?client_id=745618266361298985&permissions=8&scope=bot) to invite`)
+
+}
+
+
+module.exports.help = {
+    name: "invite",
+    description:"invite command"
+}
