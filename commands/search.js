333dd771f84a5241e001d03333b9371cf79138a4
Koenie06 <koentermaat1@gmail.com>
2020-10-03 14:02:22 +0200

    Update search.js

--- commands/search.js
+++ commands/search.js
@@ -4,7 +4,19 @@ const search = require("yt-search");
 
 module.exports.run = async (client, message, args) => {
 
-    
+    search(args.join(' '), function(err, res){
+
+        if(err) message.channel.send("There was a error to search that song!")
+
+        var videos = res.videos.slice(0, 10);
+        var response = '';
+
+        for (var i in videos) {
+
+            response += `**[${parseInt(i)+1}]: **${videos[i].title} \r\n`;
+
+        }
+    })
 
 }
 
