92c75772e3198768dd85d06bc15db18c1024ed42
Koenie06 <koentermaat1@gmail.com>
2020-10-03 13:55:47 +0200

    Create search.js

---
+++ commands/search.js
@@ -0,0 +1,10 @@
+const discord = require('discord.js');
+const bot = new discord.Client();
+
+module.exports.run = async (client, message, args) => {
+
+}
+
+module.exports.help = {
+    name: "search"
+}
