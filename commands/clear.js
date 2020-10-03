fce839cd2300092b19c36e1ba443a81c11a78f94
Koenie06 <koentermaat1@gmail.com>
2020-10-03 13:47:26 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -3,9 +3,16 @@ const bot = new discord.Client();
 
 module.exports.run = async (client, message, args) => {
 
+
+
             if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('You can\'t use this command!')
             if(!args[1]) return message.reply('Say how many messages you want to delete!').then(msg => msg.delete({ timeout: 5000}));
             message.delete()
+            if(args[1] === "all"){
+                message.channel.messages.fetch().then((results) => {
+                    message.channel.bulkDelete(results)
+                })
+            }
             message.channel.bulkDelete(args[1]);
 
 }
